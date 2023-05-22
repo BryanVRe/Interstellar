fa4e96776f7b6cd9bff48a020fe2b14442a6d710
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2023-05-22 09:10:13 -0600

    Update kafka.service.js

--- src/services/kafka.service.js
+++ src/services/kafka.service.js
@@ -1,4 +1,3 @@
-const BooksDataServiceInstance= new BooksDataService();
 
 class KafkaService {
    //url = 'https://your-kafka-express-service-kafka-adsoftsito.cloud.okteto.net/';
@@ -22,7 +21,8 @@ class KafkaService {
  
  }
  // eslint-disable-next-line
- export default BooksDataServiceInstance
+ export default new KafkaService();
+   
    
    
  
